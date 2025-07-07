a7b0e87a8bff354f0362f26e0459bfb422b9f3b6
GABRIELOFGOD <gabrielayodele2021@gmail.com>
2025-07-07 22:47:48 +0100

    GREAT IS THY FAITHFULNESS LORD UNTO ME!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import CreateMeetingPage from "@/components/create-meeting-page";
 
 export default function Home() {
   return (
-    <div className="w-full bg-gray-100 h-screen flex justify-center items-center">
+    <div>
       <CreateMeetingPage />
     </div>
   );
--- src/components/create-meeting-page.tsx
+++ src/components/create-meeting-page.tsx
@@ -142,7 +142,7 @@ const CreateMeetingPage = () => {
                 <Video className="w-5 h-5 text-white" />
               </div>
               <span className="text-xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
-                MeetSpace
+                Coinconnect MeetSpace
               </span>
             </div>
             <Badge
